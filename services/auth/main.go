8b9b4625a7a37c52249af703c03f56ef8a5995b3
jotorres <torresjoao2004@outlook.com>
2026-09-14 19:43:14 -0300

    Testando vulnerabilidade de credenciais hardcoded

--- services/auth/main.go
+++ services/auth/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"fmt"
+)
 
 func main() {
-    password := "admin123"
-    fmt.Println("auth service started", password)
+	password := "S3cr3tP@ssw0rd#2024!"
+	hash := md5.Sum([]byte(password))
+	fmt.Println("auth service started")
+	fmt.Printf("hash: %x\n", hash)
 }
